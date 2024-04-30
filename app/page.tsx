e1ec5bc121a129a08bbbfb8ce91120bbd754bd69
rohith1222004 <brohithkumar02@gmail.com>
2024-04-30 23:16:25 +0530

    popular dishes

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,208 @@
+'use client'
+
+import { ModeToggle } from "@/components/toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import { ThreeDCardDemo } from "@/components/ThreeCard";
+import { TypewriterEffectSmoothDemo } from "@/components/TypeWritting";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import { Text3DCard } from "@/components/Text3DCard";
+import { ContactForm } from "@/components/ContactForm";
+import { motion } from "framer-motion";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import { Card, CardDescription, CardContent,CardFooter,CardHeader,CardTitle } from "@/components/ui/card";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  <div className="h-full w-full  dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.05] relative items-center justify-around">
+      
+ <div className="flex items-center justify-between px-4 py-2  ">
+  {/* Left side: Logo and Restaurant Name */}
+  <div className="flex items-center space-x-2">
+    <Image src="/logo.gif" width={50} height={50} alt="logo" />
+    {/* <h1 className="text-xl font-semibold te xt-gray-600">Restaurant Name</h1> */}
+  </div>
+
+  {/* Right side: Navigation Menu */}
+  <div className="flex items-center space-x-4">
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Book</NavigationMenuTrigger>
+          <NavigationMenuTrigger>About</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Contact Us</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </div>
+</div>
+
+
+
+        {/* Hero Page */}
+        <div className="flex flex-col md:flex-row justify-around items-center">
+          <TypewriterEffectSmoothDemo/>
+
+          <div className="">
+            <ThreeDCardDemo />
+          </div>
         </div>
+        <h1 className="flex justify-center m-10 text-3xl font-semibold text-gray-600 ">Featured Items</h1>
+        
+        
+        <div className="flex justify-evenly mb-48">
+
+        {/* Carousel */}
+
+
+        <Carousel
+        className="w-1/5"
+           plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+        <CarouselContent>
+           <CarouselItem className="flex justify-center items-center">
+                      <Image src='/food_2.jpg' height={500} width={500} alt="food1"/>
+          </CarouselItem>
+          <CarouselItem className="flex justify-center items-center">
+                      <Image src='/food.jpg' height={500} width={500} alt="food1"/>
+          </CarouselItem>
+          <CarouselItem className="flex justify-center items-center">
+                      <Image src='/food_3.jpg' height={500} width={500} alt="food1"/>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+          <Text3DCard/>
+        {/* <h1 className="w-1/2 justify-center items-center flex text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </h1> */}
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* why us */}
+      <h1 className="flex justify-center m-10 text-3xl font-semibold text-gray-600 ">Why Us?</h1>
+      <div className="flex justify-center items-center">
+        <Card className="bg-gray-200 w-3/4 p-16">
+        <CardContent className="flex items-center">
+            <Image src={'/delivery.jpg'} height={150} width={150} alt="delivery" />
+            <div>
+              <h1 className="font-bold text-gray-700">Fastest Delivery</h1>
+              <p className="">It is a long established fact that a reader will be distracted by the readable </p>
+            </div>
+            <Image src={'/delivery.jpg'} height={150} width={150} alt="delivery" />
+            <div>
+              <h1 className="font-bold text-gray-700">Fastest Delivery</h1>
+              <p className="">It is a long established fact that a reader will be distracted by the readable </p>
+            </div>       
+            <Image src={'/delivery.jpg'} height={150} width={150} alt="delivery" />
+            <div>
+              <h1 className="font-bold text-gray-700">Fastest Delivery</h1>
+              <p className="">It is a long established fact that a reader will be distracted by the readable </p>
+            </div>
+        </CardContent>
+      </Card>
+    </div>
+
+      {/* Popular dishes */}
+      <h1 className="flex justify-center m-10 text-3xl font-semibold text-gray-600 ">Popular Dishes</h1>
+          <div className="flex justify-evenly">
+          <Card className="w-1/5">
+            <CardContent className="flex flex-col justify-center items-center">
+              <Image src={'/burger.jpg'} height={150} width={150} alt="food" />
+              <p className="font-semibold text-gray-700 text-center">Tasty Burger with beef and cheese dipped with white sause</p>
+            </CardContent>
+          </Card>
+          <Card className="w-1/5">
+            <CardContent className="flex flex-col justify-center items-center">
+              <Image src={'/shawarma.jpg'} height={150} width={150} alt="food"className="m-5" />
+              <p className="font-semibold text-gray-700 text-center">Tasty Burger with beef and cheese dipped with white sause</p>
+            </CardContent>
+          </Card>  
+          <Card className="w-1/5">
+            <CardContent className="flex flex-col justify-center items-center">
+              <Image src={'/pizza.jpg'} height={150} width={150} alt="food" className="m-5"/>
+              <p className="font-semibold text-gray-700 text-center">Tasty Burger with beef and cheese dipped with white sause</p>
+            </CardContent>
+          </Card>
+          </div>
+
+          <div className="flex justify-evenly mt-20">
+          <Card className="w-1/5">
+            <CardContent className="flex flex-col justify-center items-center">
+              <Image src={'/burger.jpg'} height={150} width={150} alt="food" />
+              <p className="font-semibold text-gray-700 text-center">Tasty Burger with beef and cheese dipped with white sause</p>
+            </CardContent>
+          </Card>
+          <Card className="w-1/5">
+            <CardContent className="flex flex-col justify-center items-center">
+              <Image src={'/burger.jpg'} height={150} width={150} alt="food" />
+              <p className="font-semibold text-gray-700 text-center">Tasty Burger with beef and cheese dipped with white sause</p>
+            </CardContent>
+          </Card>  
+          <Card className="w-1/5">
+            <CardContent className="flex flex-col justify-center items-center">
+              <Image src={'/burger.jpg'} height={150} width={150} alt="food" />
+              <p className="font-semibold text-gray-700 text-center">Tasty Burger with beef and cheese dipped with white sause</p>
+            </CardContent>
+          </Card>
+          </div>
+      {/* Contact Us */}
+      <h1 className="flex justify-center m-10 text-3xl font-semibold text-gray-600 ">Contact Us</h1>
+      
+      <div className="justify-center items-center flex mb-20 w-full">
+      
+      {/* <AuroraBackground> */}
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex  gap-4 items-center justify-center px-4"
+        >
+        <ContactForm/>
+        <div className="text-4xl  font-bold text-gray-700 w-96 dark:text-white text-center">
+          Contact Us to clear your doubts!
+        </div>
+      </motion.div>
+    {/* </AuroraBackground> */}
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+    </div>
   );
 }
+
+
---
+++ components/TypeWritting.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { Button } from "./ui/button";
+import { TypewriterEffectSmooth } from "./ui/typeWritting";
+import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+
+export function TypewriterEffectSmoothDemo() {
+  const words = [
+    {
+      text: "Build",
+      className:"text-7xl text-gray-700 font-bold ",
+    },
+    {
+      text: "awesome",
+      className:"text-7xl text-gray-700 font-bold ",
+    },
+  ];
+  return (
+    <div className="w-2/4 ">
+
+        <TypewriterEffectSmooth words={words} cursorClassName="bg-black-200" />
+        <p className="w-2/3 text-gray-500 font-medium">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        <Button className="mt-5">Reserver Table</Button>
+        <Button className="mt-5 ml-5 bg-gray-200  text-black">Food Menu</Button>
+        <h1 className="mt-10 mb-5 w-2/3 text-gray-500 font-bold">Trusted By</h1>
+        <Carousel
+        className="w-4/5"
+           plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+        <CarouselContent>
+          <CarouselItem className="basis-1/3">
+            <Image src={'/swiggy.svg'} height={150} width={150} alt="logo" />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Image src={'/zomato.svg'} height={150} width={150} alt="logo" />
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Image src={'/swiggy.svg'} height={150} width={150} alt="logo" />
+          </CarouselItem>     <CarouselItem className="basis-1/3">
+            <Image src={'/zomato.svg'} height={150} width={150} alt="logo" />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+  </div>
+  );
+}
